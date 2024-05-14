7404e0c6752d50727335a6e133acd857b945dac7
gustavo-jess <160682673+gustavo-jess@users.noreply.github.com>
2024-05-13 22:13:16 -0300

    Delete ProjetoContaBancaria directory

--- ProjetoContaBancaria/src/main/java/br/unipar/InterfaceContaBancaria.java
+++
@@ -1,9 +0,0 @@
-package br.unipar;
-
-public interface InterfaceContaBancaria {
-    Double obterSaldo();
-    void depositar(Double valor);
-    String retornarNumeroConta();
-    boolean saque(Double valor);
-
-}
